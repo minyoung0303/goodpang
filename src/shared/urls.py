4deeb2512f0efa2090d2261a6b75aca570320eb2
chorongfire33 <choyeon8317@naver.com>
2025-12-17 23:45:00 +0900

    feat: 인증 테스트 API 추가

--- src/shared/urls.py
+++ src/shared/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
+from user.authentication import bearer_auth
 
 base_api = NinjaAPI(title="Goodpang", version="0.0.0")
 
@@ -25,6 +26,14 @@ base_api = NinjaAPI(title="Goodpang", version="0.0.0")
 @base_api.get("")
 def health_check_handler(request):
     return {"ping": "pong"}
+
+
+@base_api.get("/auth-test", auth=bearer_auth)
+def auth_test(request):
+    return {
+        "token": request.auth,
+        "email": request.user.email,
+    }
 
 
 urlpatterns = [
