85c109fab3398792cdd46c030c6cc460426cf8ca
chorongfire33 <choyeon8317@naver.com>
2025-12-19 20:52:57 +0900

    feat: 예외처리 클래스 추가 및 auth-test api 추가

--- src/shared/urls.py
+++ src/shared/urls.py
@@ -19,12 +19,42 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 
+from user.authentication import bearer_auth
+from user.exceptions import NotAuthorizedException, UserNotFoundException
+
+
 base_api = NinjaAPI(title="Goodpang", version="0.0.0")
 
 
 @base_api.get("")
 def health_check_handler(request):
     return {"ping": "pong"}
+
+
+@base_api.get("/auth-test", auth=bearer_auth)
+def auth_test(request):
+    return {
+        "token": request.auth,
+        "email": request.user.email,
+    }
+
+
+@base_api.exception_handler(NotAuthorizedException)
+def not_authorized_exception(request, exc):
+    return base_api.create_response(
+        request,
+        {"results": {"message": exc.message}},
+        status=401,
+    )
+
+
+@base_api.exception_handler(UserNotFoundException)
+def user_not_found_exception(request, exc):
+    return base_api.create_response(
+        request,
+        {"results": {"message": exc.message}},
+        status=404,
+    )
 
 
 urlpatterns = [
